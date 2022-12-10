2f931b904d5227b518539f54b643770adc5e99bb
Robby Cuenot <51327557+robbycuenot@users.noreply.github.com>
2022-12-09 22:51:51 -0500

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -3,8 +3,4 @@
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
-ffserver -hide_banner -loglevel ${FFSERVER_LOG_LEVEL} &
-ffmpeg -hide_banner -loglevel ${FFMPEG_LOG_LEVEL} -rtsp_transport tcp ${FFMPEG_INPUT_OPTS} \
-  -i ${RTSP_URL1} -i ${RTSP_URL2} -i ${RTSP_URL3} -i ${RTSP_URL4} -r 25 \
-  -filter_complex "[0:v][1:v]hstack[top]; [2:v][3:v]hstack[bottom]; [top][bottom]vstack" \
-  ${FFMPEG_OUTPUT_OPTS} -c:v mjpeg -f ffm http://127.0.0.1:8090/feed.ffm
+vlc --video-filter wall --wall-cols 2 --wall-rows 2 ${RTSP_URL1} ${RTSP_URL2} ${RTSP_URL3} ${RTSP_URL4} :sout="#transcode{vcodec=mjpg}:std{access=http{mime=multipart/x-mixed-replace;boundary=--7b3cc56e5f51db803f790dad720ed50a},mux=mpjpeg,dst=127.0.0.1:8090}"
