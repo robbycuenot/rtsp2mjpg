41ff17450495b95e324929c9528137c32da186b2
Robby Cuenot <51327557+robbycuenot@users.noreply.github.com>
2022-12-09 21:33:00 -0500

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,4 +4,7 @@ trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
 ffserver -hide_banner -loglevel ${FFSERVER_LOG_LEVEL} &
-ffmpeg -hide_banner -loglevel ${FFMPEG_LOG_LEVEL} -rtsp_transport tcp ${FFMPEG_INPUT_OPTS} -i ${RTSP_URL1} -i ${RTSP_URL2} -i ${RTSP_URL3} -i ${RTSP_URL4} -filter_complex "nullsrc=size=1280x720 [base]; [0:v] setpts=PTS-STARTPTS [upperleft]; [1:v] setpts=PTS-STARTPTS [upperright]; [2:v] setpts=PTS-STARTPTS [lowerleft]; [3:v] setpts=PTS-STARTPTS [lowerright]; [base][upperleft] overlay=shortest=1 [tmp1]; [tmp1][upperright] overlay=shortest=1:x=480 [tmp2]; [tmp2][lowerleft] overlay=shortest=1:y=360 [tmp3]; [tmp3][lowerright] overlay=shortest=1:x=480:y=360" -c:v h264 ${FFMPEG_OUTPUT_OPTS} -f rtsp -max_muxing_queue_size 9999 http://127.0.0.1:8090/feed.ffm
+ffmpeg -hide_banner -loglevel ${FFMPEG_LOG_LEVEL} -rtsp_transport tcp ${FFMPEG_INPUT_OPTS} \
+  -i ${RTSP_URL1} -i ${RTSP_URL2} -i ${RTSP_URL3} -i ${RTSP_URL4} \
+  -filter_complex "[0:v][1:v]hstack[top]; [2:v][3:v]hstack[bottom]; [top][bottom]vstack" \
+  ${FFMPEG_OUTPUT_OPTS} http://127.0.0.1:8090/feed.ffm
