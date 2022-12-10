8b8d57b0269f8c1b548f8c455e97d6a113270e2d
Robby Cuenot <51327557+robbycuenot@users.noreply.github.com>
2022-12-09 19:18:55 -0500

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,4 +4,4 @@ trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
 ffserver -hide_banner -loglevel ${FFSERVER_LOG_LEVEL} &
-ffmpeg -hide_banner -loglevel ${FFMPEG_LOG_LEVEL} -rtsp_transport tcp ${FFMPEG_INPUT_OPTS} -i ${RTSP_URL1} -i ${RTSP_URL2} -i ${RTSP_URL3} -i ${RTSP_URL4} -filter_complex "[0:v] [0:a] [1:v] [1:a] [2:v] [2:a] [3:v] [3:a] concat=n=4:v=1:a=1 [v] [a]" -map "[v]" -map "[a]" ${FFMPEG_OUTPUT_OPTS} http://127.0.0.1:8090/feed.ffm
+ffmpeg -hide_banner -loglevel ${FFMPEG_LOG_LEVEL} -rtsp_transport tcp ${FFMPEG_INPUT_OPTS} -i ${RTSP_URL1} -i ${RTSP_URL2} -i ${RTSP_URL3} -i ${RTSP_URL4} -filter_complex "[0:v] [1:v] [2:v] [3:v] concat=n=4:v=1 [v]" -map "[v]" ${FFMPEG_OUTPUT_OPTS} http://127.0.0.1:8090/feed.ffm
